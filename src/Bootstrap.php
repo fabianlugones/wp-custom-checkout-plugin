8ed06a3eeab0b44ebd4b9eaab4825baf78994efb
Fabian Lugones <fabianlugones1@gmail.com>
2020-04-05 03:16:21 -0300

    I forgot to save some fixes in Bootstrap.php

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -15,32 +15,6 @@ class Bootstrap
         $this->initializeSettings();
     }
 
-    public function defaultOptions()
-    {
-        $defaultOptions = [
-            'wc_billing_company_priced' => 'no',
-            'wc_billing_phone_priced' => 'no',
-            'wc_billing_address_1_priced' => 'no',
-            'wc_billing_address_2_priced' => 'no',
-            'wc_billing_city_priced' => 'no',
-            'wc_billing_post_code_priced' => 'no',
-            'wc_billing_state_priced' => 'no',
-            'wc_billing_company_free' => 'no',
-            'wc_billing_phone_free' => 'no',
-            'wc_billing_address_1_free' => 'no',
-            'wc_billing_address_2_free' => 'no',
-            'wc_billing_post_code_free' => 'no',
-            'wc_billing_country_free' => 'no',
-            'wc_billing_state_free' => 'no',
-        ];
-
-        foreach ($defaultOptions as $option => $value) {
-            if (!get_option($option) || '' === get_option($option)) {
-                add_option($option, $value);
-            }
-        }
-    }
-
     public function initializeSettings()
     {
         new Settings();
